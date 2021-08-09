c27c309ab1a07d4256d2485b6204f2c8e3bf4944
foutzjesse <foutzjesse@gmail.com>
2021-08-09 07:56:22 -0400

    Update Traits.java

--- Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/Traits.java
+++ Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/Traits.java
@@ -4,6 +4,8 @@ public final class Traits
 {
 	private Traits() {}
 	
+	public static final String TABLE_NAME = "traits";
+	
 	public static final String ID = "id";
 	public static final String NAME = "name";
 	public static final String ICON = "icon";
