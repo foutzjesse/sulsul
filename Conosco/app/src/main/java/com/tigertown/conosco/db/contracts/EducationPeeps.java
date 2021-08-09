ee0cc7b9e467f85ca8586fc952d238d409ae62fb
foutzjesse <foutzjesse@gmail.com>
2021-08-09 10:40:13 -0400

    Update EducationPeeps.java

--- Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/EducationPeeps.java
+++ Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/EducationPeeps.java
@@ -15,6 +15,19 @@ public final class EducationPeeps implements IContract
 	public static final String PERSON_ID = "personid";
 	public static final String EDUCATION_ID = "educationid";
 	
+	@Override
+	public List<Pair<String, String>> GetColumns()
+	{
+		return Arrays.asList(Pair.create(PERSON_ID, DataTypes.GUID), //store id as X'123456789012...'
+			Pair.create(EDUCATION_ID, DataTypes.GUID));
+	}
+
+	@Override
+	public String GetPrimaryKey()
+	{
+		return PERSON_ID || ', ' || EDUCATION_ID;
+	}
+	
 	public static final String FK_PERSON = "fk_person";
 	public static final String FK_PERSON_TABLE = "peeps";
 	public static final String FK_PERSON_COL = "id";
