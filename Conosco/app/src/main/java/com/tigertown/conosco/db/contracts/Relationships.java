3cbbf50e5789bf8ce17741457a535289d41107e0
foutzjesse <foutzjesse@gmail.com>
2021-08-09 11:10:22 -0400

    Update Relationships.java

--- Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/Relationships.java
+++ Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/Relationships.java
@@ -12,8 +12,23 @@ public final class Relationships implements IContract
 		return TABLE_NAME;
 	}
 	
+	public static final String PERSON_ID = "personid";
 	public static final String TYPE = "type";
 	public static final String NAME_ID = "name_id";
+	
+	@Override
+	public List<Pair<String, String>> GetColumns()
+	{
+		return Arrays.asList(Pair.create(PERSON_ID, DataTypes.GUID),
+			Pair.create(TYPE, DataTypes.TEXT),
+			Pair.create(NAME_ID, DataTypes.GUID));
+	}
+
+	@Override
+	public String GetPrimaryKey()
+	{
+		return PERSON_ID + ", " + TYPE + ", " + NAME_ID;
+	}
 	
 	public static final String FK_TYPE = "fk_type";
 	public static final String FK_TYPE_TABLE = "relationshiptypes";
