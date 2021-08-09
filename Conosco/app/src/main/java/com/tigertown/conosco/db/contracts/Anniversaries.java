2992d53aef32ca1f11fcd61eb90c7224d435d554
foutzjesse <foutzjesse@gmail.com>
2021-08-09 08:01:29 -0400

    Create Anniversaries.java

---
+++ Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/Anniversaries.java
@@ -0,0 +1,12 @@
+package com.tigertown.conosco.db.contracts;
+
+public final class Anniversaries
+{
+	private Anniversaries() {}
+	
+	public static final String TABLE_NAME = "anniversaries";
+	public static final String ID = "id";
+  public static final String TYPE = "type";
+  public static final String DATE = "date";
+  public static final String NOTIFY = "notify";
+}
