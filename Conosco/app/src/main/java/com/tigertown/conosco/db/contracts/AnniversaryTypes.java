51cc224fb2b8c00a7a35479ef67de15c01c22cee
foutzjesse <foutzjesse@gmail.com>
2021-08-09 10:03:42 -0400

    Update AnniversaryTypes.java

--- Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/AnniversaryTypes.java
+++ Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/AnniversaryTypes.java
@@ -1,9 +1,16 @@
 package com.tigertown.conosco.db.contracts;
 
-public final class AnniversaryTypes
+public final class AnniversaryTypes implements IContract
 {
 	private AnniversaryTypes() {}
 	
 	public static final String TABLE_NAME = "anniversarytypes";
+	
+	@Override
+	public String GetName()
+	{
+		return TABLE_NAME;
+	}
+	
 	public static final String NAME = "name";
 }
