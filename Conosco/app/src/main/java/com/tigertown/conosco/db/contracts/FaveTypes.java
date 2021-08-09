6d7b313c70e6c39de40d942d8ec36388370adbbc
foutzjesse <foutzjesse@gmail.com>
2021-08-09 10:06:19 -0400

    Update FaveTypes.java

--- Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/FaveTypes.java
+++ Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/FaveTypes.java
@@ -1,9 +1,16 @@
 package com.tigertown.conosco.db.contracts;
 
-public final class FaveTypes
+public final class FaveTypes implements IContract
 {
 	private FaveTypes() {}
 	
 	public static final String TABLE_NAME = "favetypes";
+	
+	@Override
+	public String GetName()
+	{
+		return TABLE_NAME;
+	}
+	
 	public static final String NAME = "name";
 }
