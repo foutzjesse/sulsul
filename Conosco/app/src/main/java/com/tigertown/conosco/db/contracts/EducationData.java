992813a2550e0023929aeb150f1040830483b939
foutzjesse <foutzjesse@gmail.com>
2021-08-09 10:04:41 -0400

    Update EducationData.java

--- Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/EducationData.java
+++ Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/EducationData.java
@@ -1,8 +1,16 @@
 package com.tigertown.conosco.db.contracts;
 
-public final class EducationData
+public final class EducationData implements IContract
 {
 	private EducationData() {}
+	
+	public static final String TABLE_NAME = "educationdata";
+	
+	@Override
+	public String GetName()
+	{
+		return TABLE_NAME;
+	}
 	
 	public static final String ID = "id";
 	public static final String SCHOOL_NAME = "school";
