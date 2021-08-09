31421ffbbcb8b7c4ca5073249a30bbc7ca363254
foutzjesse <foutzjesse@gmail.com>
2021-08-09 10:16:04 -0400

    Create NamesPeeps.java

---
+++ Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/NamesPeeps.java
@@ -0,0 +1,40 @@
+package com.tigertown.conosco.db.contracts;
+import java.util.*;
+import android.util.*;
+import com.tigertown.conosco.global.*;
+
+public final class NamesPeeps implements IContract
+{
+	private NamesPeeps() {}
+	
+	public static final String TABLE_NAME = "namespeeps";
+
+	@Override
+	public String GetName()
+	{
+		return TABLE_NAME;
+	}
+	
+	public static final String PERSON_ID = "personid";
+  	public static final String NAME_ID = "nameid";
+
+	@Override
+	public List<Pair<String, String>> GetColumns()
+	{
+		return Arrays.asList(Pair.create(PERSON_ID, DataTypes.GUID), //store id as X'123456789012...'
+			Pair.create(NAME_ID, DataTypes.GUID));
+	}
+
+	@Override
+	public String GetPrimaryKey()
+	{
+		return PERSON_ID;
+	}
+
+	@Override
+	public List<ForeignKey> GetForeignKeys()
+	{
+		return Arrays.asList(new ForeignKey("fk_person", PERSON_ID, "peeps", "id"),
+			new ForeignKey("fk_name", NAME_ID, "names", "id"));
+	}
+}
