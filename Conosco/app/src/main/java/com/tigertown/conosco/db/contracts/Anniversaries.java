6f464f722f6924680ed4fe2ca99e3d181575a277
foutzjesse <foutzjesse@gmail.com>
2021-08-09 08:02:55 -0400

    Update Anniversaries.java

--- Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/Anniversaries.java
+++ Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/Anniversaries.java
@@ -6,7 +6,11 @@ public final class Anniversaries
 	
 	public static final String TABLE_NAME = "anniversaries";
 	public static final String ID = "id";
-  public static final String TYPE = "type";
-  public static final String DATE = "date";
-  public static final String NOTIFY = "notify";
+  	public static final String TYPE = "type";
+  	public static final String DATE = "date";
+  	public static final String NOTIFY = "notify";
+	
+	public static final String FK_TYPE = "fk_type";
+	public static final String FK_TYPE_TABLE = "anniversarytypes";
+	public static final String FK_TYPE_COL = "id";
 }
