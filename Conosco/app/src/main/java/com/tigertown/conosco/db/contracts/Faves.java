9ee2f36cf08fa60f1e9690feb51de570e29396a4
foutzjesse <foutzjesse@gmail.com>
2021-08-09 10:07:26 -0400

    Update Faves.java

--- Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/Faves.java
+++ Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/Faves.java
@@ -1,10 +1,17 @@
 package com.tigertown.conosco.db.contracts;
 
-public final class Faves
+public final class Faves implements IContract
 {
 	private Faves() {}
 	
 	public static final String TABLE_NAME = "faves";
+	
+	@Override
+	public String GetName()
+	{
+		return TABLE_NAME;
+	}
+	
 	public static final String ID = "id";
 	public static final String TYPE = "type";
   	public static final String VALUE = "value";
