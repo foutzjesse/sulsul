b894147fd9abc554b1d3df302873df56b72d2c51
foutzjesse <foutzjesse@gmail.com>
2021-08-09 10:09:43 -0400

    Update Peeps.java

--- Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/Peeps.java
+++ Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/Peeps.java
@@ -1,10 +1,17 @@
 package com.tigertown.conosco.db.contracts;
 
-public final class Peeps
+public final class Peeps implements IContract
 {
 	//cant instantiate it
 	private Peeps() {}
 	
 	public static final String TABLE_NAME = "peeps";
+	
+	@Override
+	public String GetName()
+	{
+		return TABLE_NAME;
+	}
+	
 	public static final String ID_COL = "id";
 }
