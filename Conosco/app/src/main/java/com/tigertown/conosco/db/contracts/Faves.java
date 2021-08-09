0528dc56b4ea4b06629e63edbdfc2f03e44b005b
foutzjesse <foutzjesse@gmail.com>
2021-08-09 08:12:50 -0400

    Update Faves.java

--- Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/Faves.java
+++ Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/Faves.java
@@ -5,10 +5,11 @@ public final class Faves
 	private Faves() {}
 	
 	public static final String TABLE_NAME = "faves";
+	public static final String ID = "id";
 	public static final String TYPE = "type";
-  public static final String VALUE = "value";
+  	public static final String VALUE = "value";
   
-  public static final String FK_TYPE = "fk_type";
-  public static final String FK_TYPE_TABLE = "favetypes";
-  public static final String FK_TYPE_COL = "name";
+  	public static final String FK_TYPE = "fk_type";
+  	public static final String FK_TYPE_TABLE = "favetypes";
+  	public static final String FK_TYPE_COL = "name";
 }
