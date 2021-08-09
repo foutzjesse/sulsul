72dc91828d7a3a3f813cadabb35afbe849a45ca5
foutzjesse <foutzjesse@gmail.com>
2021-08-09 07:48:56 -0400

    Update Relationships.java

--- Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/Relationships.java
+++ Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/Relationships.java
@@ -8,7 +8,9 @@ public final class Relationships
 	public static final String NAME_ID = "name_id";
 	
 	public static final String FK_TYPE = "fk_type";
+	public static final String FK_TYPE_TABLE = "relationshiptypes";
 	public static final String FK_TYPE_COL = "name";
 	public static final String FK_NAME = "fk_name";
-	public static final String FK_NAME_COL = "fk_name_col";
+	public static final String FK_NAME_TABLE = "names"
+	public static final String FK_NAME_COL = "id";
 }
