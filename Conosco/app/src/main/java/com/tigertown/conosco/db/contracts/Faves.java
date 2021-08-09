f583ed1be1b87655c58ebc56ab5c60a01dd5d81e
foutzjesse <foutzjesse@gmail.com>
2021-08-09 10:41:52 -0400

    Update Faves.java

--- Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/Faves.java
+++ Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/Faves.java
@@ -15,6 +15,20 @@ public final class Faves implements IContract
 	public static final String ID = "id";
 	public static final String TYPE = "type";
   	public static final String VALUE = "value";
+	
+	@Override
+	public List<Pair<String, String>> GetColumns()
+	{
+		return Arrays.asList(Pair.create(ID, DataTypes.GUID), //store id as X'123456789012...'
+			Pair.create(TYPE, DataTypes.TEXT),
+			Pair.create(VALUE, DataTypes.TEXT));
+	}
+
+	@Override
+	public String GetPrimaryKey()
+	{
+		return ID;
+	}
   
   	public static final String FK_TYPE = "fk_type";
   	public static final String FK_TYPE_TABLE = "favetypes";
