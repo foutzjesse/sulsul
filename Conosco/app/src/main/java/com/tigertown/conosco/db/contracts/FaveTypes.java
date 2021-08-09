019833073ac114da82e5cec9a742a4b45e421874
foutzjesse <foutzjesse@gmail.com>
2021-08-09 08:07:54 -0400

    Create FaveTypes.java

---
+++ Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/FaveTypes.java
@@ -0,0 +1,9 @@
+package com.tigertown.conosco.db.contracts;
+
+public final class FaveTypes
+{
+	private FaveTypes() {}
+	
+	public static final String TABLE_NAME = "favetypes";
+	public static final String NAME = "name";
+}
