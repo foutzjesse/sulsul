894c4749a5ca788bec27ab79d86943cf606e682f
foutzjesse <foutzjesse@gmail.com>
2021-08-09 10:10:28 -0400

    Update RelationshipTypes.java

--- Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/RelationshipTypes.java
+++ Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/RelationshipTypes.java
@@ -1,9 +1,16 @@
 package com.tigertown.conosco.db.contracts;
 
-public final class RelationshipTypes
+public final class RelationshipTypes implements IContract
 {
 	private RelationshipTypes() {}
 	
 	public static final String TABLE_NAME = "relationshiptypes";
+	
+	@Override
+	public String GetName()
+	{
+		return TABLE_NAME;
+	}
+	
 	public static final String NAME = "name";
 }
