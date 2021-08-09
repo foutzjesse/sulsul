12dd7cc01420c4118c361e79e840ba86ef6fb7f7
foutzjesse <foutzjesse@gmail.com>
2021-08-09 07:57:28 -0400

    Update EducationPeeps.java

--- Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/EducationPeeps.java
+++ Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/EducationPeeps.java
@@ -4,8 +4,15 @@ public final class EducationPeeps
 {
 	private EducationPeeps() {}
 	
+	public static final String TABLE_NAME = "educationpeeps";
+	
 	public static final String PERSON_ID = "personid";
 	public static final String EDUCATION_ID = "educationid";
 	
 	public static final String FK_PERSON = "fk_person";
+	public static final String FK_PERSON_TABLE = "peeps";
+	public static final String FK_PERSON_COL = "id";
+	public static final String FK_EDUCATION = "fk_education";
+	public static final String FK_EDUCATION_TABLE = "educationdata";
+	public static final String FK_EDUCATION_COL = "id";
 }
