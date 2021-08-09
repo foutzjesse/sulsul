d5690564b652b974743851e2e4cc991ff7e680f7
foutzjesse <foutzjesse@gmail.com>
2021-08-09 10:26:37 -0400

    Update AnniversariesPeeps.java

--- Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/AnniversariesPeeps.java
+++ Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/AnniversariesPeeps.java
@@ -13,7 +13,20 @@ public final class AnniversariesPeeps implements IContract
 	}
 	
 	public static final String PERSON_ID = "personid";
-  public static final String ANNIVERSARY_ID = "anniversaryid";
+  	public static final String ANNIVERSARY_ID = "anniversaryid";
+	
+	@Override
+	public List<Pair<String, String>> GetColumns()
+	{
+		return Arrays.asList(Pair.create(PERSON_ID, DataTypes.GUID), //store id as X'123456789012...'
+			Pair.create(ANNIVERSARY_ID, DataTypes.GUID));
+	}
+
+	@Override
+	public String GetPrimaryKey()
+	{
+		return PERSON_ID || ', ' || ANNIVERSARY_ID;
+	}
   
   public static final String FK_PERSON = "fk_person";
   public static final String FK_PERSON_TABLE = "peeps";
