cf94cff8200c94221db4e0aa87f5e5c6f75ef456
foutzjesse <foutzjesse@gmail.com>
2021-08-09 07:59:23 -0400

    Create AnniversaryTypes.java

---
+++ Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/AnniversaryTypes.java
@@ -0,0 +1,9 @@
+package com.tigertown.conosco.db.contracts;
+
+public final class AnniversaryTypes
+{
+	private RelationshipTypes() {}
+	
+	public static final String TABLE_NAME = "anniversarytypes";
+	public static final String NAME = "name";
+}
