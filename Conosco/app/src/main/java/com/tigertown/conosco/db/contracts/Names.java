f23c4f743d7ff83c9d63c0e130936a504d61ac7d
foutzjesse <foutzjesse@gmail.com>
2021-08-09 10:08:58 -0400

    Update Names.java

--- Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/Names.java
+++ Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/Names.java
@@ -1,11 +1,18 @@
 package com.tigertown.conosco.db.contracts;
 
-public final class Names
+public final class Names implements IContract
 {
 	//cant instantiate it
 	private Names() {}
 
 	public static final String TABLE_NAME = "names";
+	
+	@Override
+	public String GetName()
+	{
+		return TABLE_NAME;
+	}
+	
 	public static final String ID = "id";
 	public static final String FN = "fn";
 	public static final String LN = "ln";
