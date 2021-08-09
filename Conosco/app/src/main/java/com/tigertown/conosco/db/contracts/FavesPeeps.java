c5d63721ab06404006d3de9e50256a4004789f18
foutzjesse <foutzjesse@gmail.com>
2021-08-09 10:45:11 -0400

    Update FavesPeeps.java

--- Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/FavesPeeps.java
+++ Conosco/app/src/main/java/com/tigertown/conosco/db/contracts/FavesPeeps.java
@@ -13,12 +13,25 @@ public final class FavesPeeps implements IContract
 	}
 	
 	public static final String PERSON_ID = "personid";
-  public static final String FAVE_ID = "faveid";
-  
-  public static final String FK_PERSON = "fk_person";
-  public static final String FK_PERSON_TABLE = "peeps";
-  public static final String FK_PERSON_COL = "id";
-  public static final String FK_FAVE = "fk_fave";
-  public static final String FK_FAVE_TABLE = "faves";
-  public static final String FK_FAVE_COL = "id";
+	public static final String FAVE_ID = "faveid";
+	
+	@Override
+	public List<Pair<String, String>> GetColumns()
+	{
+		return Arrays.asList(Pair.create(PERSON_ID, DataTypes.GUID), //store id as X'123456789012...'
+			Pair.create(FAVE_ID, DataTypes.GUID));
+	}
+
+	@Override
+	public String GetPrimaryKey()
+	{
+		return PERSON_ID || ', ' || FAVE_ID;
+	}
+
+	public static final String FK_PERSON = "fk_person";
+	public static final String FK_PERSON_TABLE = "peeps";
+	public static final String FK_PERSON_COL = "id";
+	public static final String FK_FAVE = "fk_fave";
+	public static final String FK_FAVE_TABLE = "faves";
+	public static final String FK_FAVE_COL = "id";
 }
