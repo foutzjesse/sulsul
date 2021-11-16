1a5ea54942a28487db857008e49ce7fd22e1b1cd
foutzjesse <foutzjesse@gmail.com>
2021-11-16 10:20:00 -0500

    Create IFave.java

---
+++ Conosco/app/src/main/java/com/tigertown/conosco/global/modelInterfaces/IFave.java
@@ -0,0 +1,12 @@
+package com.tigertown.conosco.global.modelInterfaces;
+import java.time.*;
+import java.util.*;
+
+public interface IFave
+{
+	public Integer getId();
+	public String getType();
+	public void setType(String s);
+	public String getValue();
+	public void setValue(String s);
+}
