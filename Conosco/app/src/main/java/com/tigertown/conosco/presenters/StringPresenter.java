3bcf9a32b6a9209bfa7bccfb84966f6df5fd7063
foutzjesse <foutzjesse@gmail.com>
2021-11-16 10:55:00 -0500

    Create StringPresenter.java

---
+++ Conosco/app/src/main/java/com/tigertown/conosco/presenters/StringPresenter.java
@@ -0,0 +1,31 @@
+package com.tigertown.conosco.presenters;
+import com.tigertown.conosco.db.io.*;
+import com.tigertown.conosco.db.contracts.*;
+import com.tigertown.conosco.global.*;
+
+public abstract class StringPresenter<T> extends PresenterBase<String>
+{
+	private abstract T ioClient;
+	private abstract String nameColumn;
+
+	public AnniversaryTypePresenter(IView<String> v, T io, String c) {
+		super(v);
+		this.ioClient = io;
+		this.nameColumn = c;
+	}
+	
+	public void loadData(Integer id) {
+		record = (String)null;
+		this.presentData();
+	}
+	
+	public void update(String s) {
+		record = s;
+	}
+
+	@Override
+	public void save()
+	{
+		ioClient.insertSingle(nameColumn, record);
+	}
+}
