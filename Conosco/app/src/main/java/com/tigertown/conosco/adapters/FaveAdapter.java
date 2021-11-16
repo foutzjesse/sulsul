9227a7cd503dcf5bf5346c815c9dc30e73e8e50a
foutzjesse <foutzjesse@gmail.com>
2021-11-16 10:38:19 -0500

    Create FaveAdapter.java

---
+++ Conosco/app/src/main/java/com/tigertown/conosco/adapters/FaveAdapter.java
@@ -0,0 +1,33 @@
+package com.tigertown.conosco.adapters;
+import android.widget.*;
+import com.tigertown.conosco.global.modelInterfaces.*;
+import android.content.*;
+import java.util.*;
+import android.view.*;
+import com.tigertown.conosco.*;
+
+public class FaveAdapter extends ArrayAdapter<IFave>
+{
+	public FaveAdapter(Context c, List<IFave> data) {
+		super(c, 0, data);
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent)
+	{
+		IFave fave = getItem(position);
+		
+		//check if view is being reused. otherwise inflate.
+		//probably not necessary for this small list, but i don't really know.
+		if(convertView == null)
+			convertView = LayoutInflater.from(getContext()).inflate(R.layout.favelistitem, parent, false);
+			
+		TextView type = (TextView) convertView.findViewById(R.id.type);
+		TextView date = (TextView) convertView.findViewById(R.id.date);
+		
+		type.setText(fave.getType());
+		date.setText(fave.getValue());
+		
+		return convertView;
+	}
+}
