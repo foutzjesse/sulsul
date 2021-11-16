6eb85829b5b5e3f89f4e1b340f238b97b32667bc
foutzjesse <foutzjesse@gmail.com>
2021-11-16 10:56:16 -0500

    Update AnniversaryTypePresenter.java

--- Conosco/app/src/main/java/com/tigertown/conosco/presenters/AnniversaryTypePresenter.java
+++ Conosco/app/src/main/java/com/tigertown/conosco/presenters/AnniversaryTypePresenter.java
@@ -3,26 +3,9 @@ import com.tigertown.conosco.db.io.*;
 import com.tigertown.conosco.db.contracts.*;
 import com.tigertown.conosco.global.*;
 
-public class AnniversaryTypePresenter extends PresenterBase<String>
+public class AnniversaryTypePresenter extends StringPresenter<AnniversaryTypesIoClient>
 {
-	AnniversaryTypesIoClient ioClient = new AnniversaryTypesIoClient();
-
 	public AnniversaryTypePresenter(IView<String> v) {
-		super(v);
-	}
-	
-	public void loadData(Integer id) {
-		record = (String)null;
-		this.presentData();
-	}
-	
-	public void update(String s) {
-		record = s;
-	}
-
-	@Override
-	public void save()
-	{
-		ioClient.insertSingle(AnniversaryTypes.NAME, record);
+		super(v, new AnniversaryTypesIoClient(), AnniversaryTypes.NAME);
 	}
 }
