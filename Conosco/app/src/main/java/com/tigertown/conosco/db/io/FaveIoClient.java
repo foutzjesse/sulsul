bb5fe15b10b2bac5640e990da946fbb74224f6bd
foutzjesse <foutzjesse@gmail.com>
2021-11-16 10:15:26 -0500

    Create FaveIoClient.java

---
+++ Conosco/app/src/main/java/com/tigertown/conosco/db/io/FaveIoClient.java
@@ -0,0 +1,50 @@
+package com.tigertown.conosco.db.io;
+import android.database.*;
+import com.tigertown.conosco.db.contracts.*;
+import com.tigertown.conosco.dataModels.*;
+import java.util.*;
+import com.tigertown.conosco.global.modelInterfaces.*;
+import com.tigertown.conosco.global.*;
+
+public class FaveIoClient extends IdBasedIoClient<IFave, IPersonJoinRecord>
+{
+	public FaveIoClient() {
+		super(Faves.TABLE_NAME, FavesPeeps.TABLE_NAME, FavesPeeps.FAVE_ID);
+	}
+
+	@Override
+	public Fave convertRow(Cursor c)
+	{
+		int id = getInt(c, Faves.ID); // c.getInt(c.getColumnIndexOrThrow(Anniversaries.ID));
+		String type = getString(c, Faves.TYPE); //c.getString(c.getColumnIndexOrThrow(Anniversaries.TYPE));
+		String value = getString(c, Faves.VALUE); //c.getString(c.getColumnIndexOrThrow(Anniversaries.DATE));
+				
+		return new Fave(id, type, value);
+	}
+
+	@Override
+	public Fave getEmptyRecord()
+	{
+		return new Fave();
+	}
+
+	@Override
+	public HashMap<String, String> processRecordForUpsert(IFave record)
+	{
+		HashMap<String, String> result = new HashMap<String, String>();
+		
+		processId(result, record.getId());
+		
+		result.put(Faves.TYPE, record.getType());
+		result.put(Faves.VALUE, sqlize(record.getDate()));
+				
+		return result;
+	}
+	
+	@Override
+	public String getDeleteWhereClause(HashMap<String, String> data)
+	{
+		// TODO: Implement this method
+		return null;
+	}
+}
