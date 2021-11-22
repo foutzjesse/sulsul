13aca7c2ebba1223d462c4c51a0b1bb5ca0c4819
thatoneguy <foutzjesse@gmail.com>
2021-11-22 10:08:01 -0500

    add contact poc

--- Conosco/app/src/main/java/com/tigertown/conosco/views/Contact.java
+++ Conosco/app/src/main/java/com/tigertown/conosco/views/Contact.java
@@ -1,6 +1,19 @@
 package com.tigertown.conosco.views;
+import android.*;
+import android.annotation.*;
+import android.app.*;
+import android.content.*;
+import android.content.pm.*;
+import android.database.*;
+import android.net.*;
+import android.os.*;
+import android.provider.*;
+import android.support.v4.app.*;
+import android.support.v4.content.*;
+import android.view.*;
+import android.widget.*;
 
-public class Contact extends AppCompatActivity {
+public class Contact extends Activity {
 	private ImageView thumbnail;
 	private TextView contactInfo;
 	private Button button;
@@ -11,16 +24,16 @@ public class Contact extends AppCompatActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.contact);
+		setContentView(com.tigertown.conosco.R.layout.contact);
 		
-		thumbnail = findViewById(R.id.thumbnail);
-		contactInfo = findViewById(R.id.contactInfo);
-		button = findViewById(R.id.clicktopick);
+		thumbnail = (ImageView)findViewById(com.tigertown.conosco.R.id.contactthumbnail);
+		contactInfo = (TextView)findViewById(com.tigertown.conosco.R.id.contactInfo);
+		button = (Button)findViewById(com.tigertown.conosco.R.id.clicktopick);
 		
 		button.setOnClickListener(new View.OnClickListener() {
 			@Override
-			public void onCLick(View v) {
-				if(checkContactPermission()) {
+			public void onClick(View v) {
+				if(checkPermission()) {
 					pickContactIntent();
 				}
 				else {
@@ -35,7 +48,7 @@ public class Contact extends AppCompatActivity {
 	}
 	
 	private void requestPermission(){
-		String[] perm = (Manifest.permission.READ_CONTACTS);
+		String[] perm = {Manifest.permission.READ_CONTACTS};
 		
 		ActivityCompat.requestPermissions(this, perm, CONTACT_PERMISSION_CODE);
 	}
@@ -73,7 +86,7 @@ public class Contact extends AppCompatActivity {
 				c1 = getContentResolver().query(uri, null, null, null, null);
 				
 				if (c1.moveToFirst()){
-					String id = c1.getString(c1.getColumnIndex(ContactsContract.Contacts.ID));
+					String id = c1.getString(c1.getColumnIndex(ContactsContract.Contacts._ID));
 					String name = c1.getString(c1.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
 					String thumbnailUri = c1.getString(c1.getColumnIndex(ContactsContract.Contacts.PHOTO_THUMBNAIL_URI));
 					//figure out how to open contacts app
@@ -82,8 +95,8 @@ public class Contact extends AppCompatActivity {
 					
 					if (thumbnailUri != null)
 						thumbnail.setImageURI(Uri.parse(thumbnailUri));
-					else
-						thumbnail.setImageResource(R.drawable.ic_person);
+					//else
+						//thumbnail.setImageResource(R.drawable.ic_person);
 				}
 				c1.close();
 			}
