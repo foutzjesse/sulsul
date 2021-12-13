0de7e4bd4b0137cce7bcf92a1461d012741c9103
foutzjesse <foutzjesse@gmail.com>
2021-12-13 15:18:34 -0500

    Create EducationAdapter.java

---
+++ Conosco/app/src/main/java/com/tigertown/conosco/adapters/EducationAdapter.java
@@ -0,0 +1,52 @@
+package com.tigertown.conosco.adapters;
+import android.widget.*;
+import com.tigertown.conosco.global.modelInterfaces.*;
+import android.content.*;
+import java.util.*;
+import android.view.*;
+import com.tigertown.conosco.*;
+
+public class EducationAdapter extends ArrayAdapter<IEducationDatum>
+{
+	public GiftIdeaAdapter(Context c, List<IEducationDatum> data) {
+		super(c, 0, data);
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent)
+	{
+		IEducationDatum record = getItem(position);
+
+		//check if view is being reused. otherwise inflate.
+		//probably not necessary for this small list, but i don't really know.
+		if(convertView == null)
+			convertView = LayoutInflater.from(getContext()).inflate(R.layout.educationlistitem, parent, false);
+
+		TextView summary = (TextView) convertView.findViewById(R.id.edsummary);
+
+		summary.setText(composeSummary());
+
+		return convertView;
+	}
+    
+    private String composeSummary (IEducationDatum record)
+    {
+        String result = "";
+        
+        if (record.getSchool() != null)
+            result += record.getSchool()
+        
+        if (record.getYear() != null) {
+            String year = String.valueOf(record.getYear());
+            result += " " + (year.length() == 4) ? "'" + year.substring(year.length() - 2) : year;
+        }
+        
+        if ((result == "" || result.startsWith(" ")) && record.getMajor() != null)
+            result += " " + record.getMajor();
+        
+        if (result == "")
+            result = (result.getDegree()) != null ? result.getDegree() : result.getNotes();
+        
+        return result.trim();
+    }
+}
